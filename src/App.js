bc8b83434b491160d3376c45846add75ed968df0
Iyed16 <117760440+Iyed16@users.noreply.github.com>
2023-01-21 21:22:17 +0100

    first and last commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import landing from '../../pages/landing';
+
+function App() {
+  return (
+    <div>Rhayem was here .......</div>
+  );
+}
+
+export default App;
